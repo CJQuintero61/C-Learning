5e556bf3f05f3649e7e77684083f5913d59e657e
CJQuintero61 <cjquintero61@gmail.com>
2025-11-07 14:10:38 -0600

    added Hello World program

---
+++ Hello_World/main.c
@@ -0,0 +1,25 @@
+/*
+    main.c
+
+    This file is a simple hello world program.
+    The first step in learning any new language.
+
+    Created by: CJ Quintero on 11/07/2025
+    Last updated: CJ Quintero on 11/07/2025
+
+    To run:
+
+    Linux:
+    1. cc main.c
+    2. ./a.out
+    
+    Windows:
+    1.  cc main.c
+    2. .\a.exe
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello world from C!\n");
+    return 0;
+} // end main
